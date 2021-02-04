cb785f65c25dd53bce2bb3d61afdf2fe286e41fa
pchanho <61894572+pchanho@users.noreply.github.com>
2021-02-05 01:04:20 +1100

    Update block.js

--- block.js
+++ block.js
@@ -12,7 +12,7 @@ export class Block {
         const XGap = 80;
         const YGap = 60;
 
-        ctx.fillStyle = '#161e38';
+        ctx.fillStyle = '#fff';
         ctx.beginPath();
         ctx.rect(this.x, this.y, this.width, this.height);
         ctx.shadowOffsetX = 12;
@@ -21,20 +21,20 @@ export class Block {
         ctx.shadowColor = 'rgba(0, 0, 0, 0.5)';
         ctx.fill();
 
-        // ctx.fillStyle = '#190f3a';
-        // ctx.beginPath();
-        // ctx.moveTo(this.maxX, this.maxY);
-        // ctx.lineTo(this.maxX - XGap, this.maxY + YGap);
-        // ctx.lineTo(this.x - XGap, this.maxY + YGap);
-        // ctx.lineTo(this.x, this.maxY);
-        // ctx.fill();
-        //
-        // ctx.fillStyle = '#9d0919';
-        // ctx.beginPath();
-        // ctx.moveTo(this.x, this.y);
-        // ctx.lineTo(this.x, this.maxY);
-        // ctx.lineTo(this.x - XGap, this.maxY + YGap);
-        // ctx.lineTo(this.x - XGap,  this.maxY + YGap - this.height);
-        // ctx.fill();
+//         ctx.fillStyle = '#190f3a';
+//         ctx.beginPath();
+//         ctx.moveTo(this.maxX, this.maxY);
+//         ctx.lineTo(this.maxX - XGap, this.maxY + YGap);
+//         ctx.lineTo(this.x - XGap, this.maxY + YGap);
+//         ctx.lineTo(this.x, this.maxY);
+//         ctx.fill();
+        
+//         ctx.fillStyle = '#9d0919';
+//         ctx.beginPath();
+//         ctx.moveTo(this.x, this.y);
+//         ctx.lineTo(this.x, this.maxY);
+//         ctx.lineTo(this.x - XGap, this.maxY + YGap);
+//         ctx.lineTo(this.x - XGap,  this.maxY + YGap - this.height);
+//         ctx.fill();
     }
 }
